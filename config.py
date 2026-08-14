00f54d28e6b92468367fc68a0787522e844b8b03
pssongs <64870276+pssongs@users.noreply.github.com>
2026-08-14 14:41:11 +0000

    Set up population flow API configurations

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ import os
 
 load_dotenv(override=True)
 
+START_DATE = 202301
+END_DATE = 202512
+BATCH_MONTHS = 3
+
 POP_FLOW_SERVICE_KEY=os.getenv('POP_FLOW_SERVICE_KEY')
 POP_FLOW_BASE_URL="https://apis.data.go.kr/1741000/ppltnDataStus/selectPpltnDataStus"
 
