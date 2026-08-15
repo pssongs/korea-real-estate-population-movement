60f46009022fef10bca9a872aafd2deec80fef4c
pssongs <64870276+pssongs@users.noreply.github.com>
2026-08-15 05:39:55 +0000

    Build Seoul district population flow ETL pipeline

--- config.py
+++ config.py
@@ -3,10 +3,20 @@ import os
 
 load_dotenv(override=True)
 
+DB_URL = os.getenv('DB_URL')
+
 START_DATE = 202301
 END_DATE = 202512
 BATCH_MONTHS = 3
 
 POP_FLOW_SERVICE_KEY=os.getenv('POP_FLOW_SERVICE_KEY')
 POP_FLOW_BASE_URL="https://apis.data.go.kr/1741000/ppltnDataStus/selectPpltnDataStus"
-
+POP_FLOW_COLUMNS = ["statsYm",   
+        "mvinCtpvNm",
+        "mvinSggNm",
+        "mvtCtpvNm",
+        "mvtSggNm",
+        "totNmprCnt",
+        "maleNmprCnt",
+        "femlNmprCnt",        
+    ]
