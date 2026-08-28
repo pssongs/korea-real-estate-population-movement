c50167051b208a7b810d5db003581ec8fb994c21
pssongs <64870276+pssongs@users.noreply.github.com>
2026-08-28 13:48:31 +0000

    Reverse ETL existing rows update: Now updates only if value has been changed.

--- src/reverse_etl.py
+++ src/reverse_etl.py
@@ -17,7 +17,7 @@ credentials = Credentials.from_service_account_file(
 
 client = gspread.authorize(credentials)
 
-spreadsheet = client.open("Seoul Real Estate Monthly Price")
+spreadsheet = client.open_by_key('17jfM_ifrt0BltTI1l-pptB1igFcvOFGc7xirtuTNB-g')
 worksheet = spreadsheet.sheet1
 
 existing_data = worksheet.get_all_records()
@@ -36,42 +36,58 @@ with engine.begin() as conn:
 
 df = pd.DataFrame(result)
 
+# Creates a key column for the extracted data
 df['key'] = list(zip(df["district"], df["month"]))
 
+# Creates a key column for the existing data
 existing_df["key"] = list(
-    zip(existing_df["district"], existing_df["date"])
+    zip(existing_df["district"], existing_df["month"])
 )
 
 new_rows = df[~df["key"].isin(existing_df["key"])]
-existing_rows = df[df["key"].isin(existing_df["key"])]
+key_existing_rows = df[df["key"].isin(existing_df["key"])]
 
 # Dictionary to  know which row corresponds to each key
 key_to_row = {
-    (row["district"], row["date"]): i + 2
+    (row["district"], row["month"]): i + 2
     for i, row in enumerate(existing_data)
 }
 
 # Batch Updates
 updates = []
 
-for _, row in existing_rows.iterrows():
+for _, row in key_existing_rows.iterrows():
+    key = row['key']
 
-    key = row["key"]
-    sheet_row = key_to_row[key]
+    new_value = float(row['weighted_avg_price_m2'])
+    existing_value = float(
+        existing_df.loc[
+            existing_df['key'] == key,
+            'weighted_avg_price_m2'
+            ].iloc[0]
+            )
+    
+    if new_value != existing_value:
+        sheet_row = key_to_row[key]
 
-    updates.append({
-        "range": f"C{sheet_row}",
-        "values": [[row["avg_price_m2"]]]
-    })
+        updates.append({
+            "range": f"C{sheet_row}",
+            "values": [[float(new_value)]]
+        })
 
 if updates:
     worksheet.batch_update(updates)
 
-
 # Batch Inserts
 new_values = new_rows[
-    ["district", "month", "avg_price_m2"]
-].values.tolist()
+    ['district', 'month', 'weighted_avg_price_m2']
+].copy()
+
+new_values['weighted_avg_price_m2'] = (
+    new_values['weighted_avg_price_m2'].astype(float)
+)
+
+new_values = new_values.values.tolist()
 
 if new_values:
     start_row = len(existing_data) + 2
